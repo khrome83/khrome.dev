d145821c0ad2e0085290e88f4b235dcdd22727a5
Zane Milakovic <zane.milakovic@icloud.com>
2019-07-04 23:10:16 -0500

    customizes tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,74 +1,66 @@
 module.exports = {
   theme: {
     extend: {
+      colors: {
+        "regal-blue": "#243c5a"
+      },
       spacing: {
         "80": "20rem",
+        "82": "22rem",
         "108": "27rem"
+      },
+      maxHeight: {
+        "25": "25rem"
+      },
+      inset: {
+        "1/2": "25%"
       }
     },
     container: {
       padding: "1rem"
     },
     //e49c56
-    colors: {
-      background: {
-        primary: "var(--bg-background-primary)",
-        secondary: "var(--bg-background-secondary)",
-        tertiary: "var(--bg-background-tertiary)",
+    // colors: {
+    //   transparent: "transparent",
+    //   black: "#000",
+    //   white: "#fff"
 
-        form: "var(--bg-background-form)"
-      },
+    // green: {
+    //   100: "#f0fff4",
+    //   200: "#c6f6d5",
+    //   300: "#9ae6b4",
+    //   400: "#68d391",
+    //   500: "#48bb78",
+    //   600: "#38a169",
+    //   700: "#2f855a",
+    //   800: "#276749",
+    //   900: "#22543d"
+    // },
 
-      copy: {
-        primary: "var(--text-copy-primary)",
-        secondary: "var(--text-copy-hover)"
-      },
+    // orange: {
+    //   100: "#FF8F07",
+    //   300: "#FF8011",
+    //   400: "#FF6B0F",
+    //   200: "#FF8A23",
+    //   500: "#FF5E07",
+    //   600: "#ED5D10",
+    //   700: "#DD5106",
+    //   800: "#D35510",
+    //   900: "#D04900"
+    // },
 
-      "border-color": {
-        primary: "var(--border-border-color-primary)"
-      },
-
-      transparent: "transparent",
-
-      black: "#000",
-      white: "#fff",
-
-      green: {
-        100: "#f0fff4",
-        200: "#c6f6d5",
-        300: "#9ae6b4",
-        400: "#68d391",
-        500: "#48bb78",
-        600: "#38a169",
-        700: "#2f855a",
-        800: "#276749",
-        900: "#22543d"
-      },
-
-      orange: {
-        100: "#FF8F07",
-        300: "#FF8011",
-        400: "#FF6B0F",
-        200: "#FF8A23",
-        500: "#FF5E07",
-        600: "#ED5D10",
-        700: "#DD5106",
-        800: "#D35510",
-        900: "#D04900"
-      },
-
-      gray: {
-        100: "#f7fafc",
-        200: "#edf2f7",
-        300: "#e2e8f0",
-        400: "#cbd5e0",
-        500: "#a0aec0",
-        600: "#718096",
-        700: "#4a5568",
-        800: "#2d3748",
-        900: "#1a202c"
-      }
-    },
+    // gray: {
+    //   100: "#f7fafc",
+    //   200: "#edf2f7",
+    //   300: "#e2e8f0",
+    //   400: "#cbd5e0",
+    //   500: "#a0aec0",
+    //   600: "#718096",
+    //   700: "#4a5568",
+    //   800: "#2d3748",
+    //   900: "#1a202c"
+    // }
+    // },
     fontFamily: {
       sans: [
         "Lato",
@@ -95,11 +87,5 @@ module.exports = {
         "monospace"
       ]
     }
-  },
-  variants: {
-    // Some useful comment
-  },
-  plugins: [
-    // Some useful comment
-  ]
+  }
 };

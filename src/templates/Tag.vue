1dc6a59f3bbaefb6ed1ceb4eb99589919eafd3d0
Zane Milakovic <zane.milakovic@icloud.com>
2019-07-05 12:30:06 -0500

    adjusts the per page view of tags

--- src/templates/Tag.vue
+++ src/templates/Tag.vue
@@ -38,7 +38,7 @@
 query Tag ($id: String!, $page: Int) {
   tag: tag (id: $id) {
     title
-    belongsTo (page: $page, perPage: 3) @paginate {
+    belongsTo (page: $page, perPage: 10) @paginate {
       totalCount
       pageInfo {
         totalPages

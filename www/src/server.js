625e759198104c483805423137edeb0591df0892
Zane Milakovic <zane.milakovic@icloud.com>
2019-09-23 18:46:15 -0500

    fixes export to handle post processing

--- www/src/server.js
+++ www/src/server.js
@@ -3,17 +3,23 @@ import polka from "polka";
 import compression from "compression";
 import * as sapper from "@sapper/server";
 
+import initPosts from "./libs/Posts.js";
+
 import "./tailwind.css";
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === "development";
 
-polka() // You can also use Express
-  .use(
-    compression({ threshold: 0 }),
-    sirv("static", { dev }),
-    sapper.middleware()
-  )
-  .listen(PORT, err => {
-    if (err) console.log("error", err);
-  });
+new Promise(async res => {
+  res(await initPosts());
+}).then(() => {
+  polka() // You can also use Express
+    .use(
+      compression({ threshold: 0 }),
+      sirv("static", { dev }),
+      sapper.middleware()
+    )
+    .listen(PORT, err => {
+      if (err) console.log("error", err);
+    });
+});

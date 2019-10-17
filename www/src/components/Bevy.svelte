eea64618618b1cadacabe9f970982e8d87032f4d
Zane@insideRX.com <zaneinsiderx.com@IRXWM99GTFM.local>
2019-10-17 08:52:44 -0500

    handles ref and campaign tracking

--- www/src/components/Bevy.svelte
+++ www/src/components/Bevy.svelte
@@ -4,11 +4,45 @@
   let bevyToast;
 
   onMount(() => {
-    // Get Analytics Data
-    const {
-      navigator: { langage, useragent },
-      location: { hostname, href, pathname, hash, search }
+    // Default Settings
+    let hashMode = false;
+    let track = true;
+
+    // Get Basic Analytics Data From Browser
+    let {
+      navigator: { langage, userAgent, doNotTrack },
+      location: { hostname, href, pathname, hash, search },
+      document: { referrer },
     } = window;
+
+    // Get User Timezone
+    const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+    // TODO: Block Bots from userAgent
+
+    // TODO: Block Localhost
+
+    // Strip Out Ref and Campaign from Query
+    const urlParams = new URLSearchParams(search);
+    const ref = (urlParams.has('utm_source') ? urlParams.get('utm_source') : (urlParams.has('source') ? urlParams.get('source') : (urlParams.has('ref') ? urlParams.get('ref') : referrer)));
+    const campaign = (urlParams.has('utm_campaign') ? urlParams.get('utm_campaign') : (urlParams.has('campaign') ? urlParams.get('campaign') : ''));
+
+    // TODO: Check is Hash Mode
+
+
+    // TODO: Reform Correct Route, minus extra hash and query params
+
+    // TODO: Create URL for Request
+
+    // TODO: Record Page View
+
+    // TODO: Figure out how to use cache or other means to identify a new session
+
+
+
+
+
+    //----
 
     // window.navigator.language
     // 'en-US'

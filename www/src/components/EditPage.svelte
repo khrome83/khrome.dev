78a2ad309b7a5658c81da3cca5fad2302f4ef146
Zane Milakovic <zane.milakovic@icloud.com>
2019-12-09 20:34:01 -0600

    Adds EditPage component to the bottom of the blog post.

---
+++ www/src/components/EditPage.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let slug;
+</script>
+
+<div
+  class="px-8 container-inner sm:px-8 mx-auto mt-8 mb-16 lg:my-16 text-center
+  text-sm pt-8 border-t">
+  Caught a mistake or want to contribute to the blog? Edit this page on
+  <a
+    href="https://github.com/khrome83/khrome.dev/edit/master/posts/{slug}.md"
+    class="font-bold uppercase">
+    Github
+  </a>
+  !
+</div>

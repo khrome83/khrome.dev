e014270b4cd40d3094d8ea3fbf6373767a37da3f
Zane Milakovic <zane.milakovic@icloud.com>
2021-04-15 13:07:23 -0400

    defensive coding

--- utils/notion.js
+++ utils/notion.js
@@ -1,20 +1,22 @@
-import { NotionAPI } from 'notion-client';
+import { NotionAPI } from "notion-client";
 
 const headers = new Headers();
-headers.append('Authorization', `Bearer ${process.env.NOTION_TOKEN}`);
+headers.append("Authorization", `Bearer ${process.env.NOTION_TOKEN}`);
 
-const notion = new NotionAPI({authToken: process.env.NOTION_TOKEN });
+const notion = new NotionAPI({ authToken: process.env.NOTION_TOKEN });
 
 export const getAllPosts = async () => {
   const posts = await fetch(
     `https://notion-api.splitbee.io/v1/table/${process.env.NOTION_BLOG_ID}`,
     {
-      method: 'GET',
+      method: "GET",
       headers,
     }
   ).then((res) => res.json());
 
-  return (process.env.SHOW_DRAFT === "true" ? posts : posts.filter(post => post.published === true));
+  return process.env.SHOW_DRAFT === "true"
+    ? posts
+    : posts?.filter((post) => post.published === true);
 };
 
 export const getPost = async (slug) => {
@@ -24,6 +26,6 @@ export const getPost = async (slug) => {
     const blocks = await notion.getPage(post.id);
     return { blocks, post };
   } catch (e) {
-    return { blocks: {}, post: undefined};
-  }  
-}
+    return { blocks: {}, post: undefined };
+  }
+};

9f685d7531d3d66990925762ad67cd5772fe361e
Zane Milakovic <zane.milakovic@icloud.com>
2021-07-16 17:13:00 -0400

    removed amp

--- pages/_app.js
+++ pages/_app.js
@@ -4,15 +4,13 @@ import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
 
 function MyApp({ Component, pageProps }) {
-  const isAmp = useAmp();
-
   return (
     <>
       <Head>
         <meta name="author" content="Zane C. Milakovic" />
         <meta name="theme-color" content="#7c3aed" />
-        <link rel="icon" type="image/ico" href="/favicon.ico"/>
-        <link rel='manifest' href='/manifest.json'></link>
+        <link rel="icon" type="image/ico" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json"></link>
       </Head>
       <Header />
       <Component {...pageProps} />
